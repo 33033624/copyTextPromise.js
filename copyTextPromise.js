8ee4ad307f1dca430c982cb6609c60a1da0cd66e
lishupeng <lishupeng@zhuanzhuan.com>
2017-12-15 21:00:09 +0800

    first commit file

---
+++ copyTextPromise.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = input => {
+  return new Promise((resolve, reject) => {
+    const el = document.createElement('textarea');
+  	el.value = input;
+  	// Prevent keyboard from showing on mobile
+  	el.setAttribute('readonly', '');
+
+  	el.style.contain = 'strict';
+  	el.style.position = 'absolute';
+  	el.style.left = '-9999px';
+  	el.style.fontSize = '12pt'; // Prevent zooming on iOS
+
+  	const selection = document.getSelection();
+  	let originalRange = false;
+  	if (selection.rangeCount > 0) {
+  		originalRange = selection.getRangeAt(0);
+  	}
+
+  	document.body.appendChild(el);
+  	el.select();
+
+  	// Explicit selection workaround for iOS
+  	el.selectionStart = 0;
+  	el.selectionEnd = input.length;
+
+  	let success = false;
+  	try {
+  		success = document.execCommand('Copy', 'false', null) || document.execCommand('cut', 'false', null);
+  	} catch (err) {
+      resolve(err)
+  	}
+
+  	document.body.removeChild(el);
+
+  	if (originalRange) {
+  		selection.removeAllRanges();
+  		selection.addRange(originalRange);
+  	}
+    resolve(success)
+  })
+};
